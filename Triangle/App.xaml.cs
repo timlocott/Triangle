1299b2e17c1edf83394f12137388a1eb77de5f18
timlocott <tsam.cottrell@gmail.com>
2023-07-19 09:30:52 -0600

    Intialized project, main functionality implemented

---
+++ Triangle/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Triangle;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Triangle/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Triangle;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
